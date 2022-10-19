1018d9cd063c134b3f6225c9e89499c46decb851
fabit0v <fabitov@amazon.com>
2022-10-19 08:45:35 -0700

    -Initialized constructor -Created methods from nim-classes.svg -Created getters for "removed" and "remaining" -Created test file for Pile class

--- src/main/java/com/tlglearning/nim/model/Pile.java
+++ src/main/java/com/tlglearning/nim/model/Pile.java
@@ -2,14 +2,39 @@ package com.tlglearning.nim.model;
 
 public class Pile {
 
+    //Fields
     private int removed;
     private int remaining;
 
     // TODO: 10/18/22 Define constructor that initializes remove to 0 and sets remaining to the value of a parameter
 
+    //Constructor
+    public Pile(int removed, int remaining) {
+        this.removed = 0;
+        this.remaining = remaining;
+    }
+
     // TODO: 10/18/22 Define methods shown in nim-classes.svg as stubs
 
+    //Methods
+    public int remove(int quantity) {
+        return quantity;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+
     // TODO: 10/18/22 Generate "getters" (accessors) for removed and remaining
+    //Accessors (getter/setter)
+    public int getRemoved() {
+        return removed;
+    }
+
+    public int getRemaining() {
+        return remaining;
+    }
 
     // TODO: 10/18/22 Create a test class for Pile
 }
---
+++ src/test/java/com/tlglearning/nim/model/PileTest.java
@@ -0,0 +1,9 @@
+package com.tlglearning.nim.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PileTest {
+
+}
